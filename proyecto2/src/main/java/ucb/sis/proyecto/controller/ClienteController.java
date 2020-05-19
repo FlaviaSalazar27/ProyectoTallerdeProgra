2aac5e6a2950262a924386c59c5202ffa313dd94
FlaviaSalazar27 <flaviasalazarovalle@gmail.com>
2020-05-19 17:27:33 -0400

    Base de datos (sin llaves foraneas) y proyecto basico

---
+++ proyecto2/src/main/java/ucb/sis/proyecto/controller/ClienteController.java
@@ -0,0 +1,5 @@
+package ucb.sis.proyecto.controller;
+
+public class ClienteController {
+
+}
---
+++ proyecto2/src/test/java/ucb/sis/taller/Proyecto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.sis.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
