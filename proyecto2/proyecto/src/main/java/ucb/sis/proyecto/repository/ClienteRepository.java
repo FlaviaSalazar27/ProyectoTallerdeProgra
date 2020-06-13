03acc382d439f109b391b61dd4603dcfcc72880e
Nanita29 <nardaorihuela@gmail.com>
2020-06-12 23:58:07 -0400

    frontend y arreglo backend

--- proyecto2/proyecto/src/main/java/ucb/sis/proyecto/repository/ClienteRepository.java
+++ proyecto2/proyecto/src/main/java/ucb/sis/proyecto/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package ucb.sis.proyecto.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ucb.sis.proyecto.entity.Cliente;
 
